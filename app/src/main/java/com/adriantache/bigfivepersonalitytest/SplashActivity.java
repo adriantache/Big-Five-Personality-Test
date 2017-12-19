d3c29abc2af16ef30c10fe210f97f63cc59f9b79
Adrian Tache <contact@adriantache.com>
2017-12-19 10:22:34 +0200

    Add splash screen and launcher icon

---
+++ app/src/main/java/com/adriantache/bigfivepersonalitytest/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.adriantache.bigfivepersonalitytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        // close splash activity
+        finish();
+    }
+}
